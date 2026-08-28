3010c0be9d88814cfc034002be91cdbc5df5977c
seung611 <a46070766@gmail.com>
2026-08-28 13:08:33 +0900

    [Feat] 필요한 클래스 생성 및 콘솔 입력

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,23 @@
 ﻿// https://github.com/seung611/26.08.28_practice
 
 using System;
+using ConsoleApp1;
 
 class Program  
 {
     static void Main(string[] args)
     {
         
+        
+        
+        
+        // 1부터 6 사이의 번호를 받습니다. 숫자가 아니거나 범위를 벗어나면 다시 묻습니다.  
+        int menuNumber = ConsoleInput.ReadIntInRange("메뉴 번호 : ", 1, 6);
+
+        // 0 이상의 값을 받습니다. 위쪽 한계를 정하기 어려울 때 씁니다.  
+        int paid = ConsoleInput.ReadIntAtLeast("받은 금액 : ", 0);
+
+        // 결과를 보여 준 뒤 화면을 지우기 전에 잠시 멈춥니다.  
+        ConsoleInput.Pause();
     }  
 }
