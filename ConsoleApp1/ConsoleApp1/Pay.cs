02787e0481f2310e13ea1b8fb6e8be9da4edaeb6
seung611 <a46070766@gmail.com>
2026-08-28 16:26:14 +0900

    [Feat] Pay 클래스 내용 추가

--- ConsoleApp1/ConsoleApp1/Pay.cs
+++ ConsoleApp1/ConsoleApp1/Pay.cs
@@ -1,6 +1,30 @@
 ﻿namespace ConsoleApp1;
 
-public abstract class Pay
+public class Pay
 {
+    private string _SelctName;
+    private int _HowMuch;
+    private int _HowMany;
+    public int total_pay;
+    public Pay(string selectname, int price, int manynumber)
+    {
+        _SelctName = selectname;
+        _HowMuch = price;
+        _HowMany = manynumber;
+        total_pay = price * manynumber;
+    }
+    
+    public void PayPrint()
+    {
+        Console.WriteLine($"{_SelctName} x{_HowMany} {total_pay}원");
+        Console.WriteLine($"합계 : {total_pay}");
+    }
+    
+    /*if (total_pay< )
+    {
+        
+    }*/
+    
+
     
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -34,18 +34,20 @@ class Program
         }
         Console.WriteLine("----------------------------------------");
         Console.WriteLine("[장바구니]");
+        Pay pay = new ("아메리카노", 4500, 3);
+        pay.PayPrint();
         Console.WriteLine("----------------------------------------");
         Console.WriteLine("1. 담기  2. 전체 비우기  3. 결제  4. 영업 종료");
-        // int cartNumber = ConsoleInput.ReadIntInRange("번호 : ", 1, 4);
+        int cartNumber = ConsoleInput.ReadIntInRange("번호 : ", 1, 4);
         
-        
+
         // 1부터 6 사이의 번호를 받습니다. 숫자가 아니거나 범위를 벗어나면 다시 묻습니다.  
-        // int menuNumber = ConsoleInput.ReadIntInRange("메뉴 번호 : ", 1, 6);
+        int menuNumber = ConsoleInput.ReadIntInRange("메뉴 번호 : ", 1, 6);
 
         // 0 이상의 값을 받습니다. 위쪽 한계를 정하기 어려울 때 씁니다.  
-        // int paid = ConsoleInput.ReadIntAtLeast("받은 금액 : ", 0);
+        int paid = ConsoleInput.ReadIntAtLeast("받은 금액 : ", 0);
 
         // 결과를 보여 준 뒤 화면을 지우기 전에 잠시 멈춥니다.  
-        // ConsoleInput.Pause();
+        ConsoleInput.Pause();
     }  
 }
